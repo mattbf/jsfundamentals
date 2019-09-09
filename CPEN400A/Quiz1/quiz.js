4dcf1ab634c267a9fffccbb3e15dcbab9ee28010
Matthew Behan-Fossey <matthewbf8@gmail.com>
2019-09-08 21:32:13 -0700

    Solved tensortedintegrers

--- CPEN400A/Quiz1/quiz.js
+++ CPEN400A/Quiz1/quiz.js
@@ -27,12 +27,47 @@ function caseInsensitiveStringSearch( strArr, str ) {
 }
 
 //Question #2
+
+// Accepts an array of objects, consisting of two attributes: an
+// integer user id and a string email address. Returns the array of objects with the email column
+// lowercased. If the email column is missing, return a friendly error message instead.
 function lowerCaseEmails( userEmails ) {
+  //receive user objects
+  //make emails lowercase
+  //return error message if no email
+  //return array
+  // Input = [{userId: 1, email: “user1@gmail.com”}, {userId: 2, email: “User2@gmail.com”}]
+  if (userEmails && userEmails.length > 0) {
+   console.log('myArray is not empty.');
+  } else{
+     console.log('myArray is empty.');
+  }
+  // for (i=0; i<userEmails.length; i++) {
+  //   if (userEmails[i].email !== null || undefined) {
+  //     var lcaseEmail = userEmails[i].email.toLowerCase()
+  //   } else {
+  //     return "No email provided in user Object"
+  //   }
+  // }
 
 }
 
 //Question #3
 function tenSortedIntegers() {
+ //genrate 10 random intergers
+ // sort in decedning order in an array
+ var intArray = []
+
+ for (i=0; i<10; i++) {
+   intArray.push(Math.floor((Math.random() * 50) + 1))
+ }
+
+ function descendingOrder(a, b) {
+  return a > b ? -1 : b > a ? 1 : 0;
+ }
+
+  intArray.sort(descendingOrder)
+  return intArray
 
 }
 
