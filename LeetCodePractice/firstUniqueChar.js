ceb1f8fd187c79fa0409c5bba5aa18385ff751cc
Matthew Behan-Fossey <matthewbf8@gmail.com>
2019-09-17 10:03:12 -0700

    optimized first unqiue char

--- LeetCodePractice/firstUniqueChar.js
+++ LeetCodePractice/firstUniqueChar.js
@@ -1,30 +1,38 @@
 
 //given a string determine the first unqiue character (does not repeat)
 var firstUniqChar = function(s) {
-    let hashMap = [] //[{index: 0, char: "", count: 0}]
-    for (var i=0; i<s.length; i++){
-        let count = 0
-        for (var j=0; j<s.length; j++){
-            if(s[i] === s[j]){
-                count++
-            }
+let hashMap = [] //[{index: 0, char: "", count: 0}]
+for (var i=0; i<s.length; i++){
+    let count = 0
+    for (var j=0; j<s.length; j++){
+        if(s[i] === s[j]){
+            count++
         }
-        hashMap.push({index: i, char: s[i], count: count})
     }
-
-    if (hashMap.length > 0){
-        let singleChars = hashMap.filter(function(char){
-        return char.count == 1
-    })
-        if(singleChars.length > 0) {
-            return singleChars[0].index
-        } else {
-            return -1
-        }
-
-    } else{
-        return -1
+    hashMap.push({index: i, char: s[i], count: count})
+}
+//console.log(hashMap)
+for (var i=0; i<hashMap.length; i++){
+    console.log("char: " + hashMap[i].char + " count: " + hashMap[i].count)
+    if(hashMap[i].count == 1){
+        return hashMap[i].index
     }
+}
+return -1
+
+//     if (hashMap.length > 0){
+//         let singleChars = hashMap.filter(function(char){
+//         return char.count == 1
+//     })
+//         if(singleChars.length > 0) {
+//             return singleChars[0].index
+//         } else {
+//             return -1
+//         }
+
+//     } else{
+//         return -1
+//     }
 
 
 
